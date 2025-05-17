d7511975ab3b4aa5034275642fc90fc18f91b9eb
hayeon <hayaon0603@naver.com>
2025-05-18 00:32:32 +0900

    feat: add user schema

---
+++ apps/auth/src/user/user.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+export enum UserRole {
+  USER = 'USER',
+  OPERATOR = 'OPERATOR',
+  AUDITOR = 'AUDITOR',
+  ADMIN = 'ADMIN',
+}
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ type: String, enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
