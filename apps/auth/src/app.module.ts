375b98c44f28677f238a14f87588034a069d4476
hayeon <hayaon0603@naver.com>
2025-05-19 03:56:54 +0900

    feat: add setup global config and MongoDB connection add ConfigModule with Joi validation configure MongooseModule to use env integrate AuthModule and UserModule

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import Joi from 'joi';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
