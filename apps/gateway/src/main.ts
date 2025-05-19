2301ee571a219799760cdeb087514a2b793fe3fe
hayeon <hayaon0603@naver.com>
2025-05-20 06:52:31 +0900

    chore: add logging

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -7,5 +7,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
   await app.listen(port);
+  console.log(`게이트웨이 서버가 http://localhost:${port} 에서 실행 중입니다`);
 }
 bootstrap();
