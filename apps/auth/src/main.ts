bf4e0767005437384635c48bb5939ad2daf6dd5b
hayeon <hayaon0603@naver.com>
2025-05-20 09:01:29 +0900

    chore: add logging

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3001);
   await app.listen(port);
+  console.log(`인증 서버가 http://localhost:${port} 에서 실행 중입니다`);
 }
 bootstrap();
